29f0aae86068141fc2cc164dc19542c00014e02e
Hosana Abollo <hosana.abollo@hotmail.com>
2018-01-11 18:01:55 +0100

    ajout hello world

---
+++ code.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int a, res;
+	
+	printf("Donnez une valeur a \n");
+	scanf("%d",&a);
+	
+	res = a * 10;
+	
+	printf(" %d * 10 = $d",a,res);
+	printf("Hello world");
+	
+	system("pause");
+	
+	return 0;
+	
+	
+}
